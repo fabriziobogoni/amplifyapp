bd973734f75bec8bf6a2561d4a7fa735cf22c56e
Fabrizio Bogoni <fabrizio.bogoni@gmail.com>
2020-12-23 10:14:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Jodan Auth!</h1>
+        <h1>We now have Jodan 2 Auth!</h1>
       </header>
       <AmplifySignOut />
     </div>
