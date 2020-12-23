11f90303d881a278e7900aa756aa9c0933bb82c2
Fabrizio Bogoni <fabrizio.bogoni@gmail.com>
2020-12-23 10:51:11 +0100

    intrototte graphql

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,11 @@ const awsmobile = {
     "aws_cognito_region": "eu-central-1",
     "aws_user_pools_id": "eu-central-1_Vt615hRRL",
     "aws_user_pools_web_client_id": "6oiqbluog3cet1lhku8719c3rs",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://pf63hubtonfg7poy2albx2qjk4.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-central-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-44kn6qqvxvdfrgc6jzsgzdgh7e"
 };
 
 
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      name
+      description
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      name
+      description
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      name
+      description
+    }
+  }
+`;
